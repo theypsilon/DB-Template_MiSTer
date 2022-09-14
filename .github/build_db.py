9ed7b07a3047b5929ea73882d532019472658d5e
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-09-14 19:17:57 +0200

    Improving cleanup_build_py routine

--- .github/build_db.py
+++ .github/build_db.py
@@ -11,26 +11,16 @@ def main():
         print('Dry run')
         dryrun = True
 
+    db_id = os.getenv('GITHUB_REPOSITORY', 'theypsilon/test')
+
     if not dryrun:
         subprocess.run(['git', 'config', '--global', 'user.email', 'theypsilon@gmail.com'], check=True)
         subprocess.run(['git', 'config', '--global', 'user.name', 'The CI/CD Bot'], check=True)
         
-        needs_commit = False
-        if os.path.exists('build_db.py'):
-            subprocess.run(['git', 'rm', 'build_db.py'], check=True)
-            needs_commit = True
-
-        if os.path.exists('.github/build_db.py'):
-            subprocess.run(['git', 'rm', '.github/build_db.py'], check=True)
-            needs_commit = True
-
-        if needs_commit:
-            subprocess.run(['git', 'commit', '-m','Cleaning build_db.py'], check=True)
-            subprocess.run(['git', 'push'], check=True)
+        cleanup_build_py()
 
     urllib.request.urlretrieve('https://raw.githubusercontent.com/MiSTer-devel/Distribution_MiSTer/develop/.github/calculate_db.py', '/tmp/distribution_calculate_db.py')
 
-    db_id = os.getenv('GITHUB_REPOSITORY', 'theypsilon/test')
     db_url = f'https://raw.githubusercontent.com/{db_id}/db/db.json.zip'
     base_files_url = f'https://raw.githubusercontent.com/{db_id}/%s/'
 
@@ -61,6 +51,24 @@ def main():
         subprocess.run(['git', 'push', '--force','origin', 'db'], check=True)
 
     return 0
+
+def cleanup_build_py(db_id):  
+    if db_id.lower().strip() == 'theypsilon/db-template_mister':
+        print('Skipping cleanup_build_py')
+        return
+
+    needs_commit = False
+    if os.path.exists('build_db.py'):
+        subprocess.run(['git', 'rm', 'build_db.py'], check=True)
+        needs_commit = True
+
+    if os.path.exists('.github/build_db.py'):
+        subprocess.run(['git', 'rm', '.github/build_db.py'], check=True)
+        needs_commit = True
+
+    if needs_commit:
+        subprocess.run(['git', 'commit', '-m','BOT: Cleaning build_db.py'], check=True)
+        subprocess.run(['git', 'push'], check=True)
 
 if __name__ == '__main__':
     exit(main())
