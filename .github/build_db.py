9852af705aef917c747cd0769adca888b6c4d9aa
José Manuel Barroso Galindo <theypsilon@gmail.com>
2023-01-15 19:20:09 +0100

    Using db_operator instead of calculate_db script

--- .github/build_db.py
+++ .github/build_db.py
@@ -2,75 +2,106 @@ import subprocess
 import urllib.request
 import os
 import sys
+import tempfile
 
 def main():
-    print('Building database...')
+    log('Building database...')
 
     dryrun = False
     if len(sys.argv) >= 2 and sys.argv[1] == '-d':
-        print('Dry run')
+        log('Dry run')
         dryrun = True
 
     db_id = os.getenv('GITHUB_REPOSITORY', 'theypsilon/test')
 
     if not dryrun:
-        subprocess.run(['git', 'config', '--global', 'user.email', 'theypsilon@gmail.com'], check=True)
-        subprocess.run(['git', 'config', '--global', 'user.name', 'The CI/CD Bot'], check=True)
+        run(['git', 'config', '--global', 'user.email', 'theypsilon@gmail.com'])
+        run(['git', 'config', '--global', 'user.name', 'The CI/CD Bot'])
         
         cleanup_build_py(db_id)
 
-    urllib.request.urlretrieve('https://raw.githubusercontent.com/MiSTer-devel/Distribution_MiSTer/develop/.github/calculate_db.py', '/tmp/distribution_calculate_db.py')
+    log('downloading db_operator.py')
+    urllib.request.urlretrieve('https://raw.githubusercontent.com/MiSTer-devel/Distribution_MiSTer/develop/.github/db_operator.py', '/tmp/distribution_db_operator.py')
 
     db_url = f'https://raw.githubusercontent.com/{db_id}/db/db.json.zip'
     base_files_url = f'https://raw.githubusercontent.com/{db_id}/%s/'
 
-    subprocess.run(['rm *.sh'], shell=True)
+    subprocess.run(['rm *.sh'], shell=True, stderr=subprocess.STDOUT)
 
-    calculate_env = {
+    run(['python3', '/tmp/distribution_db_operator.py', 'build', '.'], env={
         'DB_ID': db_id,
         'DB_URL': db_url,
-        'DB_ZIP_NAME': 'db.json.zip',
         'DB_JSON_NAME': 'db.json',
         'BASE_FILES_URL': base_files_url,
-        'LATEST_ZIP_URL': '',
-        'CHECK_CHANGED': 'false'
-    }
+    })
 
-    print('Calculating database with environment:')
-    print(calculate_env)
-
-    result = subprocess.run(['python3', '/tmp/distribution_calculate_db.py', '-d'], env=calculate_env)
-    if result.returncode != 0:
-        print('Error: Failed to build database')
-        return 1
-
-    if not dryrun:
-        print('Pushing database...')
-        subprocess.run(['git', 'checkout', '--orphan','db'], check=True)
-        subprocess.run(['git', 'reset'], check=True)
-        subprocess.run(['git', 'add', 'db.json.zip'], check=True)
-        subprocess.run(['git', 'commit', '-m','Creating database'], check=True)
-        subprocess.run(['git', 'push', '--force','origin', 'db'], check=True)
+    if not dryrun and os.path.exists('db.json') and passes_db_tests(db_id):
+        log('Pushing database...')
+        run(['zip', 'db.json.zip', 'db.json'])
+        run(['git', 'checkout', '--orphan','db'])
+        run(['git', 'reset'])
+        run(['git', 'add', 'db.json.zip'])
+        run(['git', 'commit', '-m','Creating database'])
+        run(['git', 'push', '--force','origin', 'db'])
 
     return 0
 
+def passes_db_tests(db_id):
+    log('Testing database...')
+
+    with tempfile.TemporaryDirectory() as temp_folder:
+        log('downloading downloader.sh')
+        urllib.request.urlretrieve('https://raw.githubusercontent.com/MiSTer-devel/Downloader_MiSTer/main/downloader.sh', temp_folder + '/downloader.sh')
+        run(['chmod', '+x', 'downloader.sh'], cwd=temp_folder)
+
+        downloader_ini_content = f"""
+            [MiSTer]
+            base_path = {temp_folder}/
+            base_system_path = {temp_folder}/
+            update_linux = false
+            allow_reboot  = 0
+            verbose = false
+            downloader_retries = 0
+
+            [{db_id}]
+            db_url = {os.getcwd()}/db.json
+        """
+        log('downloader.ini content:')
+        log(downloader_ini_content)
+        
+        with open(temp_folder + '/downloader.ini', 'w') as fini:
+            fini.write(downloader_ini_content)
+
+        run(['./downloader.sh'], cwd=temp_folder, env={'DEBUG': 'true', 'CURL_SSL': ''})
+        log('The test went well.')
+        
+    return True
+
 def cleanup_build_py(db_id):  
     if db_id.lower().strip() == 'theypsilon/db-template_mister':
-        print('Skipping cleanup_build_py')
+        log('Skipping cleanup_build_py')
         return
 
     needs_commit = False
     if os.path.exists('build_db.py'):
-        subprocess.run(['git', 'rm', 'build_db.py'], check=True)
+        run(['git', 'rm', 'build_db.py'])
         needs_commit = True
 
     if os.path.exists('.github/build_db.py'):
-        subprocess.run(['git', 'rm', '.github/build_db.py'], check=True)
+        run(['git', 'rm', '.github/build_db.py'])
         needs_commit = True
 
     if needs_commit:
-        subprocess.run(['git', 'commit', '-m','BOT: Cleaning build_db.py'], check=True)
-        subprocess.run(['git', 'push'], check=True)
+        run(['git', 'commit', '-m','BOT: Cleaning build_db.py'])
+        run(['git', 'push'])
+
+def run(commands, env=None, cwd=None):
+    log(' '.join(commands))
+    if env is not None: log('with env:', env)
+    if cwd is not None: log('with cwd:', cwd)
+    subprocess.run(commands, cwd=cwd, env=env, check=True, stderr=subprocess.STDOUT)
+
+def log(*text): print(*text, flush=True)
 
 if __name__ == '__main__':
     exit(main())
