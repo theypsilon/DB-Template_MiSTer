2b72f026e9eaa71af41f64876be1aa86cc28180b
José Manuel Barroso Galindo <theypsilon@gmail.com>
2023-04-01 18:56:31 +0200

    Adding BROKEN_MRAS_IGNORE

--- .github/build_db.py
+++ .github/build_db.py
@@ -38,7 +38,8 @@ def main():
         'DB_URL': db_url,
         'DB_JSON_NAME': 'db.json',
         'BASE_FILES_URL': base_files_url,
-        'FINDER_IGNORE': os.getenv('FINDER_IGNORE', '')
+        'FINDER_IGNORE': os.getenv('FINDER_IGNORE', ''),
+        'BROKEN_MRAS_IGNORE': os.getenv('BROKEN_MRAS_IGNORE', 'true')
     })
 
     if not dryrun and os.path.exists('db.json') and passes_db_tests(db_id):
